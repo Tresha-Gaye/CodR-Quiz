7e7cc6589170391b0965561eb996379995dbda36
Twigster <treshagaye@gmail.com>
2021-12-19 20:14:49 -0500

    fixed timer bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,35 +14,37 @@ WHEN THE GAME IS OVER
 * THEN I CAN SAVE MY INITIALS AND SCORE (LOCAL STORAGE- SET ITEM, GET ITEM)*/
 
 // function to start the quiz & set timer 
-var startEl = document.querySelector("#start-btn");
+var startEl = document.getElementById("start-btn");
 
 var startQuiz = function () {
-  countdownTimer();
+  // countdownTimer();
 
 
-var timeLeft = 60;
-var countdownTimer = setInterval(function() {
+  var timeLeft = 60;
+  var countdownTimer = setInterval(function() {
+  console.log("starting");
     timeLeft--;
     document.querySelector("#quiz-max-time").textContent = timeLeft + " seconds";
       if (timeLeft <= 0) {
         clearInterval(countdownTimer);
         window.alert("Time is up! Be sure to save your score.");
-        return countdownTimer()
+        return countdownTimer
       }
     }, 1000);
   
     // return "Press start to play again"
 
-//
-startEl.addEventListener("click", countdownTimer());
+
+}; 
+startEl.addEventListener("click", startQuiz);
   
 
   // to hide/show each questions set up the data attributes for the questions
      
-startEl.addEventListener("click", function() {
-  document.querySelector("#start-btn").hidden = true;
-  document.querySelector("#question-1").hidden = false;
-}, false);
+// // startEl.addEventListener("click", function() {
+//   document.querySelector("#start-btn").hidden = true;
+//   document.querySelector("#question-1").hidden = false;
+// }, false);
 
 
 // set questions
@@ -85,85 +87,11 @@ var questionList = [
   },
 ]
 
-// var questions = questionList[i]
-// var questionsLength = questions.length
+// var questions = questionListArray
+// var questionListArrayLength = questionListArray.length
 
-for (var i = 0; i < questionsLength; i++) {
-  console.log(i);
-}
+// for (var i = 0; i < questionListArrayLength; i++) {
+//   console.log(i);
+// }
 // var randomquestion = (Math.floor(Math.random() * questionList.length));
 //     console.log(randomquestionsIndex);
-
-// var body = document.body;
-// var h1El = document.createElement("h1");
-// var infoEl = document.createElement("div");
-// var imgEl = document.createElement("img");
-// var kittenEl = document.createElement("div");
-// var nameEl = document.createElement("div");
-// var favoriteEl = document.createElement("div");
-// // Create ordered list element
-// var listEl = document.createElement("ol");
-// // Create ordered list items
-// var li1 = document.createElement("li");
-// var li2 = document.createElement("li");
-// var li3 = document.createElement("li");
-// var li4 = document.createElement("li");
-
-// h1El.textContent = "Welcome to my page";
-// kittenEl.textContent = "This is my kitten 🐱.";
-// nameEl.textContent = "His name is Jax.";
-// favoriteEl.textContent = "My favorite foods are:";
-// // Add text for list items
-// li1.textContent = "Apples 🍎 ";
-// li2.textContent = "Pizza 🍕 ";
-// li3.textContent = "Dumplings 🥟 ";
-// li4.textContent = "Cupcakes 🧁 ";
-
-// body.appendChild(h1El);
-// body.appendChild(favoriteEl);
-// favoriteEl.appendChild(listEl);
-// // Append ordered list 
-// favoriteEl.appendChild(listEl);
-// // Append list items to ordered list element 
-// listEl.appendChild(li1);
-
-
-
-// // Add styling to list element
-// listEl.setAttribute("style", "background:#333333; padding:20px;");
-// // Add styling to list items
-// li1.setAttribute("style", " color:white; background: #666666; padding: 5px; margin-left: 35px;");
-// li2.setAttribute("style", " color:white; background: #777777; padding: 5px; margin-left: 35px;");
-// li3.setAttribute("style", " color:white; background: #888888; padding: 5px; margin-left: 35px;");
-// li4.setAttribute("style", " color:white; background: #999999; padding: 5px; margin-left: 35px;");
-
-
-
-  
-// });
-
-// // to set up localStorage
-
-// var firstNameInput = document.querySelector("#first-name");
-// var lastNameInput = document.querySelector("#last-name");
-
-// signUpButton.addEventListener("click", function(event) {
-//   event.preventDefault();
-  
-//   // create user object from submission
-//   var user = {
-//     firstName: firstNameInput.value.trim(),
-//     lastName: lastNameInput.value.trim(),
-//     email: emailInput.value.trim(),
-//     password: passwordInput.value.trim()
-//   };
-
-//   // set new submission to local storage 
-//   localStorage.setItem("user", JSON.stringify(user));
-  
-// });
-
-// buttonEl.addEventListener("click", function() {
-//   alert("button clicked");
-// })
-};
